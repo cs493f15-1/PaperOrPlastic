0dd888cfe0e3e090b31de6a4d9d78a2af455aa33
unknown <sull0678@maggie.eniac.cs.pacificu.edu>
2015-10-13 12:29:39 -0700

    Added intelliJ project1

---
+++ PaperOrPlastic1/test/src/lists/GroceryList.java
@@ -0,0 +1,21 @@
+package lists;
+
+/**
+ * Created by sull0678 on 10/12/2015.
+ */
+public class GroceryList extends List
+{
+    boolean bIsLinked;
+    boolean bIsShared;
+
+    public GroceryList (String name)
+    {
+        mListName = name;
+    }
+    /*Functions
+        LinkListToGrocery (groceryList);
+        unLinkList ();
+        shareList (); (with permissions?)
+        unShareList ();
+    */
+}
---
+++ PaperOrPlastic1/test/src/lists/KitchenLists.java
@@ -0,0 +1,24 @@
+package lists; /**
+ * Created by sull0678 on 10/12/2015.
+ */
+
+
+import java.util.ArrayList;
+
+public class KitchenLists
+{
+    ArrayList<KitchenList> mLists;
+
+    public KitchenLists ()
+    {
+        mLists = new ArrayList<KitchenList>();
+    }
+
+    /*Functions
+        addList (listName);
+        deleteList (listName);
+        linkList (listName, groceryListName);
+        shareList (listName, username);
+        unShareList (listName, username);
+     */
+}
