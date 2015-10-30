c2dd5f712547109f4f0df9ddfde8985d90f09e11
unknown <alco8653@maude.eniac.cs.pacificu.edu>
2015-10-30 11:00:16 -0700

    Bri and abby's code merged

---
+++ PaperOrPlasticApp/app/src/main/java/edu/pacificu/cs493f15_1/paperorplasticapp/AboutActivity.java
@@ -0,0 +1,17 @@
+package edu.pacificu.cs493f15_1.paperorplasticapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by jone8832 on 10/26/2015.
+ */
+public class AboutActivity extends Activity
+{
+    @Override
+    protected void onCreate (Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
