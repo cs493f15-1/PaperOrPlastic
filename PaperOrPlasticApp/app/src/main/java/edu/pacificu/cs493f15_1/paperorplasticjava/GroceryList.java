52e2803a9867698999630abb88ae53e6efee5993
unknown <alco8653@maude.eniac.cs.pacificu.edu>
2015-10-30 11:30:54 -0700

    adding java files

---
+++ PaperOrPlasticApp/app/src/main/java/edu/pacificu/cs493f15_1/paperorplasticjava/GroceryList.java
@@ -0,0 +1,24 @@
+package edu.pacificu.cs493f15_1.paperorplasticjava;
+
+/**
+ * Created by sull0678 on 10/12/2015.
+ */
+public class GroceryList extends PoPList
+{
+  boolean bIsLinked;
+  boolean bIsShared;
+
+  public GroceryList (String name)
+  {
+    mListName = name;
+    mSize = 0;
+  }
+
+
+    /*Functions
+        LinkListToGrocery (groceryList);
+        unLinkList ();
+        shareList (); (with permissions?)
+        unShareList ();
+    */
+}
